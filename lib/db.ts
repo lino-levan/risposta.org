d94b2cd93b8a35d5ad0d9525534369fa9bc4a802
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-01-25 12:23:13 -0800

    chore: fix build step fr

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./supabase_types.ts";
+import { noRunOnBuild } from "./build.ts";
 
-// Avoid erroring in the build step
-if (!Deno.env.get("BUILD")) {
+noRunOnBuild(() => {
   if (!Deno.env.get("SUPABASE_URL")) {
     throw "No SUPABASE_URL in your .env";
   }
@@ -10,11 +10,12 @@ if (!Deno.env.get("BUILD")) {
   if (!Deno.env.get("SUPABASE_KEY")) {
     throw "No SUPABASE_KEY in your .env";
   }
-}
+});
 
 // Gigaweird hack to make the build step work
-export const supabase =
-  (Deno.env.get("BUILD") ? undefined : createClient<Database>(
+export const supabase = noRunOnBuild(() =>
+  createClient<Database>(
     Deno.env.get("SUPABASE_URL")!,
     Deno.env.get("SUPABASE_KEY")!,
-  ))!;
+  )
+)!;
