09888511f6a64db401ad891f3b832d9f9e613542
Trevor Gardner <trevorthebike@gmail.com>
2024-01-31 09:51:18 -0800

    added editing api. Seems to work

---
+++ routes/api/posts/[id]/edit.ts
@@ -0,0 +1,27 @@
+import { Handlers } from "$fresh/server.ts";
+import { supabase } from "lib/db.ts";
+import { getUser } from "lib/get_user.ts";
+import { bad, unauthorized } from "lib/response.ts";
+
+export const handler: Handlers = {
+  async POST(req, ctx) {
+    // TODO(lino-levan): Validate input
+    const postId = parseInt(ctx.params.id);
+    const { title, content }: { title: string; content: string } = await req
+      .json();
+
+    const user = await getUser(req);
+    if (!user) return unauthorized();
+
+    const { error } = await supabase.from("posts")
+      .update({
+        title,
+        content,
+      })
+      .eq("id", postId);
+
+    if (error) return bad();
+
+    return new Response(ctx.params.id);
+  },
+};
