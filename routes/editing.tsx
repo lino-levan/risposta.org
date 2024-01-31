16a61b9962564edc6a4fba9e9e3e86e79ff7d6d5
Trevor Gardner <trevorthebike@gmail.com>
2024-01-31 09:34:42 -0800

    fixed1

--- routes/editing.tsx
+++ routes/editing.tsx
@@ -11,9 +11,14 @@ export default function Home() {
         </div>
       </nav>
       <main class="w-screen h-screen flex flex-col justify-center items-center">
-        <h1 class="font-titan-one text-8xl text-center uppercase mb-4">Edit Posts</h1>
+        <h1 class="font-titan-one text-8xl text-center uppercase mb-4">
+          Edit Posts
+        </h1>
         <MyIsland />
-        <a href="/api/auth/editing" class="mt-4 px-4 py-2 border rounded hover:bg-gray-200 transition-all">
+        <a
+          href="/api/auth/editing"
+          class="mt-4 px-4 py-2 border rounded hover:bg-gray-200 transition-all"
+        >
           Click here
         </a>
       </main>
