abdb1271bca8425b5099661b72994d4178021b03
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-02-05 20:47:07 -0800

    feat: add classes to the dashboard

--- routes/class/index.tsx
+++ routes/class/index.tsx
@@ -1,55 +1,56 @@
 import { getUser } from "lib/get_user.ts";
 import { redirect } from "lib/response.ts";
 
+import { bad } from "lib/response.ts";
+import { supabase } from "lib/db.ts";
+
 export default async function Dashboard(req: Request) {
-  //Dummy items
-  const items = Array(15).fill(0).map((_, i) => `Question ${i + 1}`);
+  const user = await getUser(req);
+  if (!user) return redirect("/login");
+  console.log(user);
+
+  // Get member who is opening the page
+  const { data, error } = await supabase
+    .from("members")
+    .select("*, class_id!inner(*)")
+    .eq("user_id", user.id);
+  if (error) return bad();
 
   return (
     <>
       <div class="flex flex-col items-center justify-center pt-16 w-screen h-screen bg-gray-100">
         <div class="p-20 bg-white rounded flex flex-col gap-2">
           <h1 class="text-2xl font-bold">Welcome to the Dashboard</h1>
-          <a
-            class="border px-4 py-2 flex gap-2"
-            href="/class/create_class"
-          >
-            Create class as Instructor
-          </a>
-          <a
-            class="border px-4 py-2 flex gap-2"
-            href="/class/join_class"
-          >
-            Join class as Student (not implemented)
-          </a>
+          <div class="grid grid-cols-2 gap-2">
+            <a
+              class="border flex-grow px-4 py-2 flex justify-center hover:bg-gray-100"
+              href="/class/create_class"
+            >
+              Create class
+            </a>
+            <a
+              class="border flex-grow px-4 py-2 flex justify-center hover:bg-gray-100"
+              href="/class/join_class"
+            >
+              Join class
+            </a>
+          </div>
+          {data.map(({ class_id }) => (
+            <a
+              href={`/class/${class_id.id}`}
+              class="border px-4 py-2 flex items-center justify-between hover:bg-gray-100"
+            >
+              <img
+                class="w-8 h-8 rounded-full"
+                src={`https://api.dicebear.com/7.x/initials/svg?seed=${
+                  encodeURIComponent(class_id.name)
+                }`}
+              />
+              <p>{class_id.name}</p>
+            </a>
+          ))}
         </div>
       </div>
     </>
   );
 }
-/*
-import { getUser } from "lib/get_user.ts";
-import { redirect } from "lib/response.ts";
-
-export default async function Home(req: Request) {
-  const user = await getUser(req);
-  if (user) return redirect("/class");
-
-  return (
-    <>
-      <header class="w-screen p-4 shadow fixed flex gap-4">
-        <a href="/" class="font-titan-one text-xl uppercase">Risposta</a>
-        <div class="ml-auto" />
-        <a href="/signup">Sign Up</a>
-        <a href="/login">Log In</a>
-      </header>
-      <div class="w-screen h-screen flex flex-col justify-center items-center">
-        <h1 class="font-titan-one text-8xl text-center uppercase">Risposta</h1>
-        <p class="text-center">
-          The education platform you've been waiting for.
-        </p>
-      </div>
-    </>
-  );
-}
-*/
