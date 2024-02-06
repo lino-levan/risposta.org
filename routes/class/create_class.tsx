d6a12d7826f6d2fb4601d8b11ba35d5fc588ad07
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-02-05 20:53:49 -0800

    chore: update styling for class dashboard

--- routes/class/create_class.tsx
+++ routes/class/create_class.tsx
@@ -1,14 +1,12 @@
-import CreateClassForm from "../../islands/CreateClass.tsx";
-import { getUser } from "lib/get_user.ts";
-import { redirect } from "lib/response.ts";
+import CreateClassForm from "islands/CreateClass.tsx";
 
-export default async function createClass(req: Request) {
+export default async function CreateClass(req: Request) {
   return (
-    <>
-      <div class="w-screen h-screen flex flex-col justify-center items-center gap-4">
-        <h1>Create a New Class</h1>
+    <div class="flex flex-col items-center justify-center pt-16 w-screen h-screen bg-gray-100">
+      <div class="p-20 bg-white rounded flex flex-col gap-2">
+        <h1 class="text-2xl font-bold">Create a New Class</h1>
         <CreateClassForm />
       </div>
-    </>
+    </div>
   );
 }
