69880abadfab6c337b188a7044110834b4fe02b0
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-02-08 10:11:05 -0800

    chore: fix bug with posting

--- routes/class/[classId]/create.tsx
+++ routes/class/[classId]/create.tsx
@@ -1,7 +1,7 @@
 import { PostQuestion } from "islands/PostQuestion.tsx";
 import { RouteContext } from "$fresh/server.ts";
 
-export default function Create(req: Request, ctx: RouteContext) {
+export default async function Create(req: Request, ctx: RouteContext) {
   return (
     <div class="p-8 bg-white rounded">
       <h1 class="text-4xl pb-4 font-bold">Create Post</h1>
