e72a6d919b6570484126bfd87d661615ca9d96fe
Trevor Gardner <trevorthebike@gmail.com>
2024-02-12 08:32:59 -0800

    merging to upstream

--- routes/class/[classId]/post/[postId].tsx
+++ routes/class/[classId]/post/[postId].tsx
@@ -3,10 +3,10 @@ import { getUser } from "lib/get_user.ts";
 import { redirect, unauthorized } from "lib/response.ts";
 import { supabase } from "lib/db.ts";
 import { Vote } from "islands/Vote.tsx";
-import { EditPost } from "islands/edit.tsx";
 import { bad } from "lib/response.ts";
-import { DeletePost } from "islands/delete.tsx";
 import { PostComment } from "islands/PostComment.tsx";
+import { DeletePost } from "islands/delete.tsx";
+import { EditPost } from "islands/edit.tsx";
 
 export default async function Dashboard(req: Request, ctx: RouteContext) {
   // TODO(lino-levan): Clean up
@@ -43,6 +43,24 @@ export default async function Dashboard(req: Request, ctx: RouteContext) {
     ? 0
     : (data[0].upvote ? 1 : -1);
 
+  //comments for the current post
+  const { data: comments } = await supabase
+    .from("comments")
+    .select("*")
+    .eq("post_id", ctx.params.postId);
+
+  if (error) {
+    console.error("Failed to fetch comments:", error);
+    return; // Or handle the error as appropriate for your application
+  }
+
+  const { data: postCreator, error1 } = await supabase
+  .from("posts")
+  .select("*, member:member_id(user_id)")
+  .eq("id", ctx.params.postId)
+  .single();
+const postCreatorId = postCreator.member.user_id;
+
   return (
     <div class="w-full h-full p-4 flex flex-col gap-2">
       <div class="bg-white p-4 rounded">
@@ -60,6 +78,20 @@ export default async function Dashboard(req: Request, ctx: RouteContext) {
           </div>
         </div>
         <p class="pl-8">{post.content}</p>
+        <EditPost
+          postId={post.id}
+          initialTitle={post.title}
+          initialContent={post.content}
+          classId={ctx.params.classId}
+          userId={user.id}
+          postCreatorId={postCreatorId}
+        />
+        <DeletePost
+          postId={post.id}
+          userId={user.id}
+          classId={ctx.params.classId}
+          postCreatorId={postCreatorId}
+        />
         {comments.map((comment) => (
           <div class="border px-4 py-2 flex items-center justify-between">
             <p>{comment.content}</p>
