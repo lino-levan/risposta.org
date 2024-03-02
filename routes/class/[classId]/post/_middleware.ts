87279692ac3da72515748a9b3ec64fe7d124cf9d
Yiming Zeng <yzeng54@ucsc.edu>
2024-03-01 16:18:58 -0800

    formatting fix

--- routes/class/[classId]/post/_middleware.ts
+++ routes/class/[classId]/post/_middleware.ts
@@ -6,10 +6,11 @@ export async function handler(
   _: Request,
   ctx: FreshContext<PostState>,
 ) {
-  const { data: postData, error } = await supabase.from("expanded_posts").select("*").eq(
-    "id",
-    ctx.params.postId,
-  ).eq("class_id", ctx.params.classId);
+  const { data: postData, error } = await supabase.from("expanded_posts")
+    .select("*").eq(
+      "id",
+      ctx.params.postId,
+    ).eq("class_id", ctx.params.classId);
   if (error || !postData || postData.length === 0) return ctx.renderNotFound();
   ctx.state.post = postData[0];
   return await ctx.next();
