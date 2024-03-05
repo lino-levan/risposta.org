7abbda095781086874f1c2d07e29a78c9654fa1a
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-03-04 18:37:23 -0800

    chore: redesign homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,85 +7,65 @@ export default async function Home(req: Request) {
 
   return (
     <div className="bg-gray-100 min-h-screen">
-      {/* Header Section */}
-      <header className="w-screen p-4 shadow fixed flex items-center justify-between bg-green-100">
+      <header className="w-screen p-4 fixed flex items-center justify-between bg-green-100 z-10">
         <div className="flex items-center max-w-screen-md">
-          {/* Adjust width */}
           <a href="/" className="font-titan-one text-xl uppercase ml-12">
             Risposta
           </a>
-          <nav className="ml-8">
-            <a href="/tab1" className="mx-2 text-gray-700 hover:text-blue-700">
-              Tab 1
-            </a>
-            <a href="/tab2" className="mx-2 text-gray-700 hover:text-blue-700">
-              Tab 2
-            </a>
-            <a href="/tab2" className="mx-2 text-gray-700 hover:text-blue-700">
-              Tab 3
-            </a>
-            <a href="/tab2" className="mx-2 text-gray-700 hover:text-blue-700">
-              Tab 4
-            </a>
-            {/* Add more tabs as needed */}
-          </nav>
         </div>
-        <div className="mr-4">
+        <div className="mr-4 flex gap-4">
           <a
             href="/signup"
-            className="mr-2 text-gray-700 hover:text-blue-700 mr-4"
+            className="text-gray-700 hover:text-gray-500"
           >
             Sign Up
           </a>
-          <a href="/login" className="text-gray-700 hover:text-blue-700 mr-4">
+          <a href="/login" className="text-gray-700 hover:text-gray-500">
             Log In
           </a>
         </div>
       </header>
 
-      {/* Hero Section */}
-      <section className="bg-gradient-to-b from-green-500 to-blue-400 text-white py-72 px-12 text-center">
-        <div className="container mx-auto max-w-screen-md">
-          <h1 className="text-7xl font-bold mb-8">Welcome to Risposta</h1>
+      <section className="bg-green-100 text-green-800 py-72 px-12 text-center lg:text-left w-screen overflow-hidden relative">
+        <div className="container max-w-screen-md">
+          <h1 className="text-6xl font-bold mb-8">Welcome to Risposta</h1>
           <p className="text-2xl mb-10">
             The education platform you've been waiting for.
           </p>
-          <div className="flex justify-center space-x-8">
-            {/* Adjust the space between buttons */}
+          <div className="flex space-x-8 justify-center lg:justify-start">
             <a
               href="/signup"
-              className="btn btn-primary border rounded-lg flex items-center px-8 py-2 text-xl font-semibold text-black bg-white hover:bg-blue-400"
+              className="rounded-lg flex items-center px-8 py-2 text-xl font-semibold text-black bg-white hover:bg-gray-300"
             >
               Continue as Student
             </a>
             <a
               href="/signup"
-              className="btn btn-secondary border rounded-lg flex items-center px-8 py-2 text-xl font-semibold text-black bg-white hover:bg-blue-400"
+              className="rounded-lg flex items-center px-8 py-2 text-xl font-semibold text-black bg-white hover:bg-gray-300"
             >
               Continue as Teacher
             </a>
           </div>
         </div>
+        <img
+          src="/screenshot.png"
+          class="hidden lg:block absolute h-[600px] rounded-lg shadow-lg left-2/3 top-24"
+        />
       </section>
 
-      {/* key features */}
       <section className="py-16 px-8 text-center bg-white">
         <div className="container mx-auto max-w-screen-md">
-          {/* Adjust width */}
           <h2 className="text-3xl font-bold mb-4">Key Features</h2>
           <div className="flex justify-between">
             <div className="w-1/3">
-              {/* Image 1 */}
               <img src="image1.jpg" alt="Image 1" className="w-full mb-4" />
               <p className="text-lg">Description for Image 1</p>
             </div>
             <div className="w-1/3">
-              {/* Image 2 */}
               <img src="image2.jpg" alt="Image 2" className="w-full mb-4" />
               <p className="text-lg">Description for Image 2</p>
             </div>
             <div className="w-1/3">
-              {/* Image 3 */}
               <img src="image3.jpg" alt="Image 3" className="w-full mb-4" />
               <p className="text-lg">Description for Image 3</p>
             </div>
@@ -93,10 +73,8 @@ export default async function Home(req: Request) {
         </div>
       </section>
 
-      {/* About Section */}
       <section className="py-16 px-8 text-center">
         <div className="container mx-auto max-w-screen-md">
-          {/* Adjust width */}
           <h2 className="text-3xl font-bold mb-4">About Us</h2>
           <p className="text-lg mb-8">
             Risposta is designed to empower both students and teachers in the
@@ -105,37 +83,9 @@ export default async function Home(req: Request) {
           </p>
         </div>
       </section>
-
-      {/* Features Section */}
-      <section className="bg-white py-16 px-8 text-center">
-        <div className="container mx-auto max-w-screen-md">
-          {/* Adjust width */}
-          <h2 className="text-3xl font-bold mb-4">Extra tab1</h2>
-          <p className="text-lg mb-8">
-            Discover the features that make Risposta the perfect choice for your
-            educational needs.
-          </p>
-          {/* Add feature highlights here */}
-        </div>
-      </section>
-
-      {/* Testimonials Section */}
-      <section className="py-16 px-8 text-center">
-        <div className="container mx-auto max-w-screen-md">
-          {/* Adjust width */}
-          <h2 className="text-3xl font-bold mb-4">Extra tab2</h2>
-          <p className="text-lg mb-8">
-            descriptions2
-          </p>
-          {/* Add testimonials here */}
-        </div>
-      </section>
-
-      {/* Footer Section */}
       <footer className="bg-green-100 text-gray-700 py-8 px-4 text-center">
         <div className="container mx-auto max-w-screen-md">
-          {/* Adjust width */}
-          <p>&copy; 2024 Risposta. All rights reserved.</p>
+          <p>© 2024 Risposta Authors. All rights reserved.</p>
         </div>
       </footer>
     </div>
--- routes/signup.tsx
+++ routes/signup.tsx
@@ -8,48 +8,25 @@ export default async function Signup(req: Request) {
 
   return (
     <div className="bg-green-300 min-h-screen flex flex-col">
-      {/* Top Bar */}
       <header className="w-screen p-4 shadow fixed flex items-center justify-between bg-green-100">
         <a href="/" className="font-titan-one text-xl uppercase ml-12">
           Risposta
         </a>
-        <div className="mr-4">
-          <a href="/" className="mr-2 text-gray-700 hover:text-blue-700 mr-4">
-            Back to Home
-          </a>
-        </div>
       </header>
 
-      {/* Signup Form */}
-      <div className="flex-grow flex justify-center items-center">
-        <div
-          className="w-3/5 bg-white rounded-lg shadow-md p-16 relative"
-          style={{ height: "70vh" }}
-        >
-          {/* Add the link here */}
-          <a
-            href="/"
-            className="font-titan-one text-3xl uppercase absolute top-5 left-10"
-          >
-            Risposta
-          </a>
-
-          {/* Content aligned to the middle of the left 2/3 part */}
-          <div className="absolute inset-y-0 left-0 flex flex-col justify-center items-center w-2/3 h-full">
-            <h1 className="text-6xl mb-17">Sign up</h1>
-            <p className="text-lg text-center mb-28">
+      <div className="flex-grow flex justify-center items-center p-4">
+        <div className="max-w-screen-md w-full bg-white rounded-lg shadow-md p-16 py-48 relative">
+          <div className="flex flex-col gap-2 items-center h-full">
+            <h1 className="text-6xl text-center">Sign up</h1>
+            <p className="text-lg text-center">
               We're happy you're here.
             </p>
             <a
               href="/api/auth/login"
-              className="px-4 py-2 border rounded flex items-center gap-4 hover:bg-gray-200 transition-all absolute bottom-1/2 transform translate-y-1/2"
+              className="px-4 py-2 border rounded flex items-center gap-4 w-max hover:bg-gray-200 transition-all"
             >
               <GoogleIcon /> Continue with Google
             </a>
-          </div>
-
-          {/* Grey area */}
-          <div className="absolute top-0 right-0 h-full w-1/3 bg-gray-200">
           </div>
         </div>
       </div>
