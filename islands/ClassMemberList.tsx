a808a599d22fe237722acafbe44c540877131978
Jimmy3498 <123849820+Jimmy3498@users.noreply.github.com>
2024-03-10 17:27:34 -0700

    feat: add member list (#33)
    * resolving conflicts
    
    * member list
    
    * formatting

---
+++ islands/ClassMemberList.tsx
@@ -0,0 +1,130 @@
+import { Database } from "lib/supabase_types.ts";
+import { bad } from "lib/response.ts";
+
+interface UserDetail {
+  name: string;
+  picture: string;
+}
+
+export interface MemberListProps {
+  classId: number;
+  members: (Database["public"]["Tables"]["members"]["Row"] & {
+    user_id: UserDetail;
+  })[];
+}
+
+function groupMembersByRole(members: MemberListProps["members"]) {
+  return members.reduce(
+    (acc: Record<string, MemberListProps["members"]>, member) => {
+      const roleGroup = acc[member.role] || [];
+      roleGroup.push(member);
+      acc[member.role] = roleGroup;
+      return acc;
+    },
+    {},
+  );
+}
+
+export function MemberList(props: MemberListProps) {
+  const members = props.members;
+
+  const groupedMembers = groupMembersByRole(members);
+
+  const handleAction = async (
+    memberId: number,
+    action: "remove" | "makeTA" | "removeTA",
+  ) => {
+    let method = "";
+    let body = {};
+
+    switch (action) {
+      case "remove":
+        //remove from class
+        method = "DELETE";
+        body = { id: memberId };
+        break;
+      case "makeTA":
+      case "removeTA": {
+        //change TA/student role
+        method = "PATCH";
+        const role = action === "makeTA" ? "TA" : "student";
+        console.log("role of switch to: ", role);
+        body = { id: memberId, role: role };
+        break;
+      }
+      default:
+        return;
+    }
+
+    const response = await fetch("/api/class/[id]/member", {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+      //refresh page when succeed
+      window.location.reload();
+    } else {
+      return bad();
+    }
+  };
+
+  return (
+    <div class="p-4">
+      {Object.entries(groupedMembers).map(([role, members]) => (
+        <div key={role} class="card bg-base-100 shadow-xl mb-6">
+          <div class="card-body">
+            <h2 class="card-title capitalize">{role} ({members.length})</h2>
+            <div class="divider"></div> {/* Divider for visual separation */}
+            <div class="flex flex-wrap gap-4 justify-start">
+              {/* Members aligned horizontally */}
+              {members.map((member) => (
+                <div
+                  key={member.id}
+                  class="dropdown dropdown-end bg-base-200 shadow hover:shadow-lg transition-shadow duration-200 rounded-badge"
+                >
+                  <div tabindex={0} class="flex gap-4 items-center">
+                    <img
+                      src={member.user_id.picture}
+                      alt="Profile Picture"
+                      class="w-10 h-10 rounded-full"
+                    />
+                    <p>{member.user_id.name}</p>
+                  </div>
+                  <ul
+                    tabindex={0}
+                    class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+                    style={{ zIndex: 50 }}
+                  >
+                    <li>
+                      <a onClick={() => handleAction(member.id, "remove")}>
+                        Remove from Class
+                      </a>
+                    </li>
+                    {role !== "TA" && role !== "instructor" && (
+                      <li>
+                        <a onClick={() => handleAction(member.id, "makeTA")}>
+                          Make TA
+                        </a>
+                      </li>
+                    )}
+                    {role === "TA" && (
+                      <li>
+                        <a onClick={() => handleAction(member.id, "removeTA")}>
+                          Remove TA Role
+                        </a>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
