a4f460059d990dad662959d4f8fe90d9b6385326
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-03-12 14:02:29 -0700

    chore: fix lint error

--- lib/ai.ts
+++ lib/ai.ts
@@ -1,11 +1,14 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const API_KEY = Deno.env.get("GEMINI_KEY");
+
 /**
  * If AI is available, return the AI response, otherwise return null.
  */
 export async function generateResponse(prompt: string): Promise<string | null> {
+  if (!API_KEY) return null;
   try {
-    const ai = new GoogleGenerativeAI(Deno.env.get("GEMINI_KEY"));
+    const ai = new GoogleGenerativeAI(API_KEY);
     const model = ai.getGenerativeModel({ model: "gemini-pro" });
     const result = await model.generateContent(prompt);
     return await result.response.text();
