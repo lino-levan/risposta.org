d05b5cb29c8847929f9cef69edf0f4966819956f
Harrison Zhou <yzhou253@ucsc.edu>
2024-03-11 22:41:36 -0700

    edit username, Class ui change, changed user profile ui

--- routes/api/user/editname.ts
+++ routes/api/user/editname.ts
@@ -3,16 +3,16 @@ import { supabase } from "lib/db.ts";
 import { APIState } from "lib/state.ts";
 import { bad, success } from "lib/response.ts";
 
-// TODO(lino-levan): Validate inputs
+
 export const handler: Handlers<unknown, APIState> = {
   async POST(req, ctx) {
     // Get user information
     const user = ctx.state.user;
 
-    // Class id thats passed through
+    // new username thats passed through
     const { newUsername } = await req.json();
 
-    // Insert new member
+    
     // Update the username with the new name
     const { error: updateError } = await supabase.from("users")
       .update({ name: newUsername })
--- routes/class/user.tsx
+++ routes/class/user.tsx
@@ -19,10 +19,10 @@ export default async function Dashboard(
           className="w-3/6 bg-white rounded-lg shadow-md p-16 relative"
           style={{ height: "60vh" }}
         >
-          <div className="absolute left-0 top-0 h-full w-1/3 bg-blue-500 rounded-l-lg flex justify-center items-center" style={{ zIndex: 1 }}>
+          <div className="absolute left-0 top-0 h-full w-1/3 bg-blue-500 rounded-l-lg flex justify-center" style={{ zIndex: 1 }}>
             <div className="text-white text-center">
-                <img src={user.picture} alt="User Profile" className="w-40 h-40 ml-3 mb-6" />
-                <h1 className="text-3xl">{user.name}</h1>
+                <img src={user.picture} alt="User Profile" className="w-40 h-40 ml-3 mb-12 mt-24" />
+                <h1 className="text-3xl mb-24">{user.name}</h1>
                 <div className="mt-4">
                     <a className="btn btn-error text-lg" href="/api/auth/logout" style={{ pointerEvents: 'auto' }}>
                         <IconLogout /> Log out
@@ -31,29 +31,30 @@ export default async function Dashboard(
             </div>
         </div>
           <div className="relative pl-8">
-            <h1 className="text-4xl ml-64 mb-4">User Profile</h1>
+            <h1 className="text-4xl ml-64 mb-2">User Profile</h1>
+            <hr className="border-b-2 border-gray-400 mb-0 ml-64" />
             <div className="flex items-center gap-12 p-12">
-              <div className="flex flex-col ml-52">
+            <div className="flex flex-col ml-52">
                 <h1 className="text-3xl mb-4">Email</h1>
-                <h2 className="text-lg ml-1 mb-10">{user.email}</h2>
-                <h1 className="text-3xl mb-16">Others</h1>
-                
-              </div>
-              
+                <h2 className="text-lg ml-1 mb-12">{user.email}</h2>
+                <h1 className="text-3xl mb-4">Others</h1>
+                <hr className="border-b-2 border-gray-400 mb-12 w-full" />
+            </div>
+            
               {/* Themes button */}
-              <div className="absolute right-0 bottom-2 mr-60">
+              <div className="absolute right-0 bottom-2 mr-64">
                 <Themes theme={cookies.theme ?? "cupcake"} />
               </div>
               {/* Edit button */}
               <div className="absolute right-0 bottom-0 mb-3 mr-96">
-                <a className="btn btn-primary" href="/class/edit_name">Edit User Name</a>
-              </div>
-              {/* Back button */}
-              <div className="absolute right-0 bottom-0 mb-3 mr-4" >
-                <a className="btn btn-primary" href="/class">Go Back</a>
+                <a className="btn btn-primary mr-3" href="/class/edit_name">Edit User Name</a>
               </div>
             </div>
           </div>
+          {/* Back button */}
+          <div className="absolute right-0 bottom-0 mb-12 mr-16" >
+                <a className="btn btn-primary" href="/class">Go Back</a>
+              </div>
         </div>
       </div>
     </div>
